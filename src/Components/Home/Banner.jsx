05d4d4f0fe3c1b408e08a117abee67fff693f9d1
rasel khan <tunnerbd2016@gmail.com>
2023-11-05 17:35:22 +0600

    banne img overlay remove

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -13,8 +13,9 @@ const Banner = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: false,
+        autoplaySpeed: 4000,
+        pauseOnHover: true,
+
     };
     useEffect(() => {
         AOS.init(
@@ -23,21 +24,18 @@ const Banner = () => {
     }, [])
 
     return (
-        <div data-aos="fade-up" className="w-full" >
+        <div data-aos="fade-up"
+
+            className="w-full" >
             <Slider className=" mx-auto  h-[80vh] " {...settings}>
 
                 {/* banner img */}
 
-                <div className="relative">
+                <div className="">
                     <img className="rounded-xl w-full h-[75vh]" src="https://i.ibb.co/41t5SwD/asian-boy-library-room-school.jpg" alt="Slide 1" />
-                    <div className="absolute  top-0 right-0 w-1/2 h-full bg-black opacity-50 rounded-xl flex flex-col justify-center items-center">
-                        <h2 className="text-4xl font-bold text-white mb-4">Discover a World of Knowledge</h2>
-                        <div className="flex space-x-4">
-                            <button className="bg-[#77B748] text-white px-6 py-3 rounded-full">Borrow</button>
-                            <button className="bg-[#71AE44] text-white px-6 py-3 rounded-full">Read</button>
-                        </div>
-                    </div>
+
                 </div>
+
                 <div>
                     <img className=" rounded-xl w-full h-[75vh]" src="https://i.ibb.co/kq7H2Ww/library-with-books.jpg" />
                 </div>
